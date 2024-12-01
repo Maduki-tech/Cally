972c9f2a5179171885e3dad51b74e51d62d2ecbf
Maduki-tech <d.schlueter1011@gmail.com>
2024-12-01 21:57:48 +0100

    added basic project structure

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,17 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
+  tabWidth: 4,
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: "all",
+  arrowParens: "always",
+  semi: true,
+  useTabs: false,
+  endOfLine: "lf",
+  quoteProps: "as-needed",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  htmlWhitespaceSensitivity: "strict",
+  vueIndentScriptAndStyle: false,
   plugins: ["prettier-plugin-tailwindcss"],
 };
